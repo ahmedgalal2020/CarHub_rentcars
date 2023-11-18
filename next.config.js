7f8b8fed5f920386e1cacdbceec6fbea5eb7af40
ahmedgalal2020 <Galal.trading2015@gmail.com>
2023-11-18 18:17:05 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["cdn.imagin.studio"]
-    }
+    output: 'export',
+
 }
 
 module.exports = nextConfig
